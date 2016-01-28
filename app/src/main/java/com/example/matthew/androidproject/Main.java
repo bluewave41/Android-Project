bec4664d20474fa46e181b07dee9d0c42c677322
bluewave41 <megaman22342@live.ca>
2016-01-28 14:33:35 -0800

    Finished

--- app/src/main/java/com/example/matthew/androidproject/Main.java
+++ app/src/main/java/com/example/matthew/androidproject/Main.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -13,6 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -22,10 +26,10 @@ public class Main extends AppCompatActivity {
 
     CanvasView view1;
     CanvasView view2;
+    SeekBar bar;
     boolean image = false;
     int frameCount = 5;
-    ArrayList<Drawable> pictures = new ArrayList<Drawable>();
-    int index = 4; //sketchy ass shit sorry dennis
+    ArrayList<Bitmap> pictures = new ArrayList<Bitmap>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,20 @@ public class Main extends AppCompatActivity {
         getSupportActionBar().setTitle("Project");
         view1 = (CanvasView)findViewById(R.id.view);
         view2 = (CanvasView)findViewById(R.id.view2);
+        bar = (SeekBar)findViewById(R.id.seekBar);
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar){}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ImageView view = (ImageView)findViewById(R.id.imageView);
+                Drawable display = new BitmapDrawable(getResources(), pictures.get(progress));
+                view.setBackground(display);
+            }
+        });
         view1.setTwin(view2);
         view2.setTwin(view1);
     }
@@ -104,15 +122,18 @@ public class Main extends AppCompatActivity {
         }
     }
 
-    public void hello(View view) {
-        view1.setBackground(pictures.get(index--)); //this shit is backwards ok? dont ask plz
-    }
-
-    public ArrayList<Drawable> createBitmap(CanvasView src, ArrayList<Difference> difference) {
+    /**Creates bitmaps representing each frame of the warp.
+     *
+     * @param src Source image view
+     * @param dst Destination image view
+     * @param difference Difference between line positions
+     * @return Arraylist of each generated frame.
+     */
+    public ArrayList<Bitmap> createBitmap(CanvasView src, CanvasView dst, ArrayList<Difference> difference) {
         Bitmap blank;
         Drawable view1Immutable = src.getBackground(); //get both backgrounds
         Bitmap view1Background = ((BitmapDrawable) view1Immutable).getBitmap();
-        ArrayList<Drawable> frames = new ArrayList<>();
+        ArrayList<Bitmap> frames = new ArrayList<>();
 
         view1Background = Bitmap.createScaledBitmap(view1Background, 400, 400, false);
         double newX, newY;
@@ -122,9 +143,9 @@ public class Main extends AppCompatActivity {
             ArrayList<Line> temp = new ArrayList();
             blank = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888);
             for (int q = 0; q < view1.lines.size(); q++) {
-                Line srcLine = src.lines.get(q);
+                Line srcLine = dst.lines.get(q);
                 Difference d = difference.get(q);
-                Line l = new Line(srcLine.startX + Math.round(d.X1 * i), srcLine.startY + Math.round(d.Y1 * i), srcLine.stopX + Math.round(d.X2 * i), srcLine.stopY + Math.round(d.Y2 * i));
+                Line l = new Line(srcLine.startX - Math.round(d.X1 * i), srcLine.startY - Math.round(d.Y1 * i), srcLine.stopX - Math.round(d.X2 * i), srcLine.stopY - Math.round(d.Y2 * i));
                 temp.add(l);
             }
 
@@ -138,15 +159,13 @@ public class Main extends AppCompatActivity {
                         Line srcLine = temp.get(l);
                         double ptx = dest.startX - x;
                         double pty = dest.startY - y;
-                        double pqx = dest.stopX - dest.startX;
-                        double pqy = dest.stopY - dest.startY;
                         double nx = dest.yLength * -1;
                         double ny = dest.xLength;
 
                         double d = ((nx * ptx) + (ny * pty)) / ((Math.sqrt(nx * nx + ny * ny))); //clean up all this shit since half of it probably isn't needed
-                        double fp = ((pqx * (ptx * -1)) + (pqy * (pty * -1)));
-                        fp = fp / (Math.sqrt(pqx * pqx + pqy * pqy));
-                        fp = fp / (Math.sqrt(pqx * pqx + pqy * pqy));
+                        double fp = ((dest.xLength * (ptx * -1)) + (dest.yLength * (pty * -1)));
+                        fp = fp / (Math.sqrt(dest.xLength * dest.xLength + dest.yLength * dest.yLength));
+                        fp = fp / (Math.sqrt(dest.xLength * dest.xLength + dest.yLength * dest.yLength));
 
                         nx = srcLine.yLength * -1;
                         ny = srcLine.xLength;
@@ -182,55 +201,76 @@ public class Main extends AppCompatActivity {
                     blank.setPixel(x, y, view1Background.getPixel((int) Math.abs(newX), (int) Math.abs(newY)));
                 }
             }
-            view1Immutable = new BitmapDrawable(getResources(), blank);
-            frames.add(view1Immutable);
+            frames.add(blank);
         }
         return frames;
     }
 
-    public void crossDisolve(ArrayList<Drawable> frames1, ArrayList<Drawable> frames2) {
+    /**Slowly dissolves picture 1 into picture 2.
+     *
+     * @param frames1 Frames of image 1 to image 2
+     * @param frames2 Frames of image2 to image 1
+     */
+    public void crossDissolve(ArrayList<Bitmap> frames1, ArrayList<Bitmap> frames2) {
         int width = view1.getWidth();
         int height = view1.getHeight();
         int factor1 = 1;
         int factor2 = frameCount-factor1;
-        for(int i=0; i <frames1.size();i++) {
-            Drawable image1 = frames1.get(i);
-            Drawable image2 = frames2.get(i);
-            Bitmap view1Background = ((BitmapDrawable) image1).getBitmap();
-            Bitmap view2Background = ((BitmapDrawable) image2).getBitmap();
+        for(int i=0; i < frames1.size();i++) {
+            Bitmap view1Background = frames1.get(i);
+            Bitmap view2Background = frames2.get(i);
             Bitmap blank = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888);
             for(int x=0;x<width;x++) {
                 for(int y=0;y<height;y++) {
                     int pixel1 = view1Background.getPixel(x, y);
                     int pixel2 = view2Background.getPixel(x, y);
-                    blank.setPixel(x, y, ((pixel1*(factor1/frameCount))+(pixel2*(factor2/frameCount))));
+                    int red = Color.red(pixel1)*factor2/frameCount+Color.red(pixel2)*factor1/frameCount;
+                    int green = Color.green(pixel1)*factor2/frameCount+Color.green(pixel2)*factor1/frameCount;
+                    int blue = Color.blue(pixel1)*factor2/frameCount+Color.blue(pixel2)*factor1/frameCount;
+                    int pixel = Color.rgb(red, green, blue);
+                    blank.setPixel(x, y, pixel);
                 }
             }
-            image1 = new BitmapDrawable(getResources(), blank);
-            pictures.add(image1);
+            pictures.add(blank);
             factor1++;
             factor2 = frameCount-factor1;
         }
     }
 
-    public void morph(View view) {
-        ArrayList<Difference> difference = new ArrayList();
-        ArrayList<Difference> difference2 = new ArrayList();
-        ArrayList<Drawable> frames1, frames2;
-
+    /**Generate value to increment each line by to create intermediate frames.
+     *
+     * @param difference Arraylist to store increment value for destination to source image.
+     * @param difference2 Arraylist to store increment value for source to destination image.
+     */
+    public void generateIntermediateFrames(ArrayList<Difference> difference, ArrayList<Difference> difference2) {
         for(int i=0;i<view1.lines.size();i++) {
             Line dest = view2.lines.get(i);
             Line src = view1.lines.get(i);
-            Difference d = new Difference((dest.startX-src.startX)/(frameCount+1), (dest.stopX-src.stopX)/(frameCount+1), (dest.startY-src.startY)/(frameCount+1), (dest.stopY-src.stopY)/(frameCount+1));
+            Difference d = new Difference((float)(dest.startX-src.startX)/(frameCount+1), (float)(dest.stopX-src.stopX)/(frameCount+1), (float)(dest.startY-src.startY)/(frameCount+1), (float)(dest.stopY-src.stopY)/(frameCount+1));
             difference.add(d);
-            d = new Difference((src.startX-dest.startX)/(frameCount+1), (src.stopX-dest.stopX)/(frameCount+1), (src.startY-dest.startY)/(frameCount+1), (src.stopY-dest.stopY)/(frameCount+1));
+            d = new Difference(((float)src.startX-dest.startX)/(frameCount+1), (float)(src.stopX-dest.stopX)/(frameCount+1), (float)(src.startY-dest.startY)/(frameCount+1), (float)(src.stopY-dest.stopY)/(frameCount+1));
             difference2.add(d);
         }
+    }
 
-        frames1 = createBitmap(view1, difference);
-        frames2 = createBitmap(view2, difference2);
-        //pictures = frames1;
-        crossDisolve(frames1, frames2);
+    /**Morphs image 1 into image 2.
+     *
+     * @param view
+     */
+    public void morph(View view) {
+        TextView text = (TextView)findViewById(R.id.textView);
+        pictures.add(((BitmapDrawable) view1.getBackground()).getBitmap());
+        ArrayList<Difference> difference = new ArrayList();
+        ArrayList<Difference> difference2 = new ArrayList();
+        ArrayList<Bitmap> frames1, frames2;
+        text.setText("Generating frames...");
+        generateIntermediateFrames(difference, difference2); //generate linear difference for intermediate frames
+        text.setText("Warping image 1...");
+        frames1 = createBitmap(view1, view2, difference); //generate frames for picture 1 to 2
+        text.setText("Warping image 2");
+        frames2 = createBitmap(view2, view1, difference2); //geneate frames for picture 2 to 2
+        text.setText("Cross dissolving...");
+        crossDissolve(frames1, frames2); //cross disolve the frames together
     }
 
     @Override
